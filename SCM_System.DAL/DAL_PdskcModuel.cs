b3e16a20fa82ae2e3768a68629d5aafaad5b1b6d
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-24 16:50:07 +0800

    blue-pink Buy模块

--- SCM_System.DAL/DAL_PdskcModuel.cs
+++ SCM_System.DAL/DAL_PdskcModuel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 using SCM_System.Model;
 namespace SCM_System.DAL
 {
     public class DAL_PdskcModuel<T> : DAL_UniversalModuel<T>, IDAL.IDAL_UniversalModuel<T> where T : BaseModel
     {
+
     }
 }
