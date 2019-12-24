c490bd90057535f8d91d4b74f58635a3762cd29c
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-24 14:31:25 +0800

    Revert "Revert "2019年12月24日 02点39分""
    This reverts commit 6b5399592d0b0fed63887299aee608b091dbd3c2.

---
+++ SCM_System.UI/Controllers/BuyController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM_System.UI.Controllers
+{
+    public class BuyController : Controller
+    {
+        // GET: Buy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Depot()
+        {
+            return View();
+        }
+
+        public ActionResult DepotAdd()
+        {
+            return View();
+        }
+
+        public ActionResult DepotDetail()
+        {
+            return View();
+        }
+
+        public ActionResult DepotEdit()
+        {
+            return View();
+        }
+
+        public ActionResult PurchaseReturn()
+        {
+            return View();
+        }
+
+        public ActionResult PurchaseReturnAdd()
+        {
+            return View();
+        }
+
+        public ActionResult PurchaseReturnDetail()
+        {
+            return View();
+        }
+        public ActionResult PurchaseReturnEdit()
+        {
+            return View();
+        }
+        public ActionResult Reserve()
+        {
+            return View();
+        }
+
+        public ActionResult ReserveDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Stocks()
+        {
+            return View();
+        }
+
+        public ActionResult StocksAdd()
+        {
+            return View();
+        }
+
+        public ActionResult StocksDetail(string sid)
+        {
+            ViewBag.sid = sid;
+            return View();
+        }
+
+        public ActionResult StocksEdit()
+        {
+            return View();
+        }
+    }
+}
