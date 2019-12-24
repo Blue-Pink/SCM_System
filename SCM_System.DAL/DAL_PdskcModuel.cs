114569dd0d31a785a3834c7ef37d5cc08f049e18
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-24 17:16:21 +0800

    111

--- SCM_System.DAL/DAL_PdskcModuel.cs
+++ SCM_System.DAL/DAL_PdskcModuel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 using SCM_System.Model;
 using SCM_System.IDAL;
 using Ninject;
