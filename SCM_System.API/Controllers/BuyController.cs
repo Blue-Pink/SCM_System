eb89bde97dbb92eb08c6ea4d376b13884dab557e
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-25 16:40:29 +0800

    Revert "1"
    This reverts commit 900818afc62ce36ca7f9a6b170d9aa45b9c41820.

--- SCM_System.API/Controllers/BuyController.cs
+++ SCM_System.API/Controllers/BuyController.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Web.Http;
 using SCM_System.Model;
 using System.Threading.Tasks;
-using SCM_System.DAL;
 
 namespace SCM_System.API.Controllers
 {
@@ -17,28 +16,12 @@ namespace SCM_System.API.Controllers
 
         [Inject]
         public DAL.DAL_BuyModuel<BaseModel> buyModuel { get; set; }
-        #region 表名标注
-        //P     -> 商品表(Products)
-        //S     -> 采购单(Stocks)
-        //PL   -> 供货商(ProductLend)
-        //U    -> 用户表(Users)
-        //DS  -> 库存表(DepotStock) 
-        //D    -> 仓库(Depots)
-        //PT   -> 商品类别(ProductTypes)
-        #endregion
+
         [HttpGet]
         [Route("GetS_PL_Us")]
         public async Task<dynamic> GetS_PL_Us()
         {
             return await buyModuel.GetS_PL_Us().ConfigureAwait(false);
-        }
-
-        [HttpGet]
-        [Route("GetDS_P_Ds")]
-        public async Task<dynamic> GetDS_P_Ds()
-        {
-            var temp = await buyModuel.GetDS_P_D().ConfigureAwait(false);
-            return temp;
         }
 
         public void Options() { }  //这是预请求
