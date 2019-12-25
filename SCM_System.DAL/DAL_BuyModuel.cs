fc7f9aa708515678666548cc519fdc102f90044e
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-25 16:43:51 +0800

    Revert "Revert "1""
    This reverts commit eb89bde97dbb92eb08c6ea4d376b13884dab557e.

--- SCM_System.DAL/DAL_BuyModuel.cs
+++ SCM_System.DAL/DAL_BuyModuel.cs
@@ -7,14 +7,19 @@ using System.Threading.Tasks;
 using SCM_System.Model;
 namespace SCM_System.DAL
 {
-    public class DAL_BuyModuel<T> : DAL_UniversalModuel<T>, IDAL.IDAL_UniversalModuel<T> where T : BaseModel
+    public class DAL_BuyModuel<T> : DAL_UniversalModuel<T>, IDAL.IDAL_UniversalModuel<T> where T : class
     {
-        //!表明标注: P -> 商品表(Products)
+        #region 表名标注
+        //P     -> 商品表(Products)
+        //S     -> 采购单(Stocks)
+        //PL   -> 供货商(ProductLend)
+        //U    -> 用户表(Users)
+        //DS  -> 库存表(DepotStock) 
+        //D    -> 仓库(Depots)
+        //PT   -> 商品类别(ProductTypes)
+        #endregion
 
-        //表明标注: S -> 采购单(Stocks)
-        //表明标注: PL -> 供货商(ProductLend)
-        //表明标注: U -> 用户表(Users)
-
+        #region 示例代码
         public async Task<Dictionary<string, dynamic>> GetS_PL_Us()
         {
             var temp = await entities.Stocks.Join(entities.ProductLend,
@@ -41,12 +46,47 @@ namespace SCM_System.DAL
                     S.供货商名称,
                     S.下单时间,
                     S.预计交货时间,
-                    创建人=U.UsersName,
+                    创建人 = U.UsersName,
                     S.审核状态,
                     S.备注
                 }
                 ).ToListAsync();
             return new Dictionary<string, dynamic>() { { "data", temp } };
+        } 
+        #endregion
+
+        public async Task<Dictionary<string,dynamic>> GetDS_P_D()
+        {
+            var temp = await entities.DepotStock.OrderBy(DS=>DS.DSID).Join(entities.Products,
+                DS => DS.ProID,
+                P => P.ProID,
+                (DS, P) => new {
+                    DS.DSID,
+                    P.ProName,
+                    P.ProWorkShop,
+                    P.PTID,
+                    P.ProPrice,
+                    DS.DSAmount,
+                    P.ProInPrice,
+                    P.ProMax,
+                    P.ProMin,
+                }).Join(entities.ProductTypes,
+                    DS_P=>DS_P.PTID,
+                    PT=>PT.PTID,
+                    (DS_P,PT)=>new {
+                        DS_P.DSID,
+                        DS_P.ProName,
+                        DS_P.ProWorkShop,
+                        DS_P.ProPrice,
+                        DS_P.DSAmount,
+                        DS_P.ProInPrice,
+                        PT.PTName,
+                        DS_P.ProMax,
+                        DS_P.ProMin,
+                    }
+                )
+                .ToListAsync();
+            return new Dictionary<string, dynamic>() { { "data",temp} };
         }
     }
 }
--- SCM_System.DAL/DAL_UniversalModuel.cs
+++ SCM_System.DAL/DAL_UniversalModuel.cs
@@ -11,7 +11,7 @@ using System.Web.Script.Serialization;
 
 namespace SCM_System.DAL
 {
-    public class DAL_UniversalModuel<T> : IDAL_UniversalModuel<T> where T : BaseModel
+    public class DAL_UniversalModuel<T> : IDAL_UniversalModuel<T> where T : class
     {
         [Inject]
         public SCMEntities entities { get; set; }
@@ -34,6 +34,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 内主键进行删除
+        /// </summary>
+        /// <param name="key"> T 内主键</param>
+        /// <returns>操作结果</returns>
         public async Task<int> Delete_Key(dynamic key)
         {
             try
@@ -50,6 +55,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 内多属性进行删除
+        /// </summary>
+        /// <param name="properties">T 内属性,键值格式为: {"属性名","条件值"}</param>
+        /// <returns>操作结果</returns>
         public async Task<int> Delete_Properties(Dictionary<string, dynamic> properties)
         {
             try
@@ -91,6 +101,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 进行数据添加
+        /// </summary>
+        /// <param name="model">具体数据</param>
+        /// <returns>操作结果</returns>
         public async Task<int> Insert(T model)
         {
             try
@@ -105,6 +120,10 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 获取对应所有数据
+        /// </summary>
+        /// <returns>结果集</returns>
         public async Task<List<T>> Select_All()
         {
             try
@@ -119,6 +138,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 内主键获取对应所有数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>结果集</returns>
         public Task<T> Select_Key(dynamic key)
         {
             try
@@ -133,6 +157,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 内属性获取对应所有数据
+        /// </summary>
+        /// <param name="properties">T 内属性,键值格式为: {"属性名","条件值"}</param>
+        /// <returns>结果集</returns>
         public async Task<List<T>> Select_Properties(Dictionary<string, dynamic> properties)
         {
             try
@@ -176,6 +205,12 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// 针对 T 内属性进行修改
+        /// </summary>
+        /// <param name="properties">T 内属性,键值格式为: {"属性名","条件值"}</param>
+        /// <param name="model">修改所需新数据</param>
+        /// <returns>操作结果</returns>
         public async Task<int> Update_Properties(Dictionary<string, dynamic> properties, T model)
         {
             //try
@@ -226,6 +261,12 @@ namespace SCM_System.DAL
             //}
         }
 
+        /// <summary>
+        /// 针对 T 内主键进行修改
+        /// </summary>
+        /// <param name="key">T 内主键</param>
+        /// <param name="model">修改所需新数据</param>
+        /// <returns>操作结果</returns>
         public async Task<int> Update_Key(dynamic key, T model)
         {
             try
@@ -266,6 +307,10 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// (无条件)针对 T 获取对应数据条数
+        /// </summary>
+        /// <returns>数据条数</returns>
         public async Task<int> ObtainCount_All()
         {
             try
@@ -278,6 +323,11 @@ namespace SCM_System.DAL
             }
         }
 
+        /// <summary>
+        /// (据条件)针对 T 获取对应数据条数
+        /// </summary>
+        /// <param name="properties">T 内属性,键值格式为: {"属性名","条件值"}</param>
+        /// <returns>数据条数</returns>
         public async Task<int> ObtainCount_Properties(Dictionary<string, dynamic> properties)
         {
             try
