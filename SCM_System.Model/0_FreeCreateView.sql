ba38eca0a57cdafdf7d088c681bb6a8491964d17
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-26 20:45:27 +0800

    OK

--- SCM_System.Model/0_FreeCreateView.sql
+++ SCM_System.Model/0_FreeCreateView.sql
@@ -1,4 +1,15 @@
-﻿create view V_DS_P_PT
+﻿--P         -> 商品表(Products)
+--S         -> 采购单(Stocks)
+--PL       -> 供货商(ProductLend)
+--U        -> 用户表(Users)
+--DS      -> 库存表(DepotStock) 
+--D        -> 仓库(Depots)
+--PT       -> 商品类别(ProductTypes)
+
+if(exists(select * from sysobjects where name='V_DS_P_PT'))
+drop view V_DS_P_PT
+go
+create view V_DS_P_PT
 as 
 select DS_P.DSID,
                         DS_P.ProName,
@@ -23,10 +34,10 @@ Products p on ds.ProID=p.ProID) ds_p  left join
 ProductTypes pt on ds_p.PTID=pt.PTID
 go
 
-if(exists(select * from sysobjects where name='V_DS_P_PT'))
-drop view V_DS_P_PT
+if(exists(select * from sysobjects where name='V_CD_CDD'))
+drop view V_CD_CDD
 go
-create view V_DS_P_PT
+create view V_CD_CDD
 as 
 select cd.CDID,
 cd.DepotID,
@@ -35,26 +46,3 @@ cd.UserID,
 'diff'=(cdd.CDDAmount1-cdd.DevAmount2),
 cdd.ProID,cd.CDDesc,
 cd.CDState from CheckDepot cd left join CheckDepotDetail cdd on cd.CDID=cdd.CDID
-create view V_DS_P_PT
-as 
-select DS_P.DSID,
-                        DS_P.ProName,
-                        DS_P.ProWorkShop,
-                        DS_P.ProPrice,
-                        DS_P.DSAmount,
-                        DS_P.ProInPrice,
-                        DS_P.ProMax,
-                        DS_P.ProMin,
-                        pt.PTName from 
-(select  DS.DSID,
-                    P.ProName,
-                    P.ProWorkShop,
-                    P.PTID,
-                    P.ProPrice,
-                    DS.DSAmount,
-                    P.ProInPrice,
-                    P.ProMax,
-                    P.ProMin
-                    from DepotStock ds left join 
-Products p on ds.ProID=p.ProID) ds_p  left join	
-ProductTypes pt on ds_p.PTID=pt.PTID
