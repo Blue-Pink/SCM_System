6f69f6925768babd983c654674485d367b20e330
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-26 17:28:14 +0800

    ABD

--- SCM_System.Model/0_FreeCreateView.sql
+++ SCM_System.Model/0_FreeCreateView.sql
@@ -1,28 +1,4 @@
-﻿create view V_DS_P_PT
-as 
-select DS_P.DSID,
-                        DS_P.ProName,
-                        DS_P.ProWorkShop,
-                        DS_P.ProPrice,
-                        DS_P.DSAmount,
-                        DS_P.ProInPrice,
-                        DS_P.ProMax,
-                        DS_P.ProMin,
-                        pt.PTName from 
-(select  DS.DSID,
-                    P.ProName,
-                    P.ProWorkShop,
-                    P.PTID,
-                    P.ProPrice,
-                    DS.DSAmount,
-                    P.ProInPrice,
-                    P.ProMax,
-                    P.ProMin
-                    from DepotStock ds left join 
-Products p on ds.ProID=p.ProID) ds_p  left join	
-ProductTypes pt on ds_p.PTID=pt.PTID
-go
-
+﻿
 if(exists(select * from sysobjects where name='V_DS_P_PT'))
 drop view V_DS_P_PT
 go
