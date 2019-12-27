9e80943f7dd08468b7cf90bb6ab4c1fab3316bac
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2019-12-27 11:04:51 +0800

    1

--- SCM_System.API/Controllers/BuyController.cs
+++ SCM_System.API/Controllers/BuyController.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using SCM_System.Model;
 using System.Threading.Tasks;
-
+using SCM_System.DAL;
 namespace SCM_System.API.Controllers
 {
     [RoutePrefix("api/buy")]
