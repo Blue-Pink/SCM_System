322539a7d675c3bc043fa379776d60dcdbbee80b
Blue-Pink <50697511+Blue-Pink@users.noreply.github.com>
2020-01-13 13:48:32 +0800

    11

--- SCM_System.API/Controllers/SystemController.cs
+++ SCM_System.API/Controllers/SystemController.cs
@@ -1,14 +1,53 @@
-﻿using System;
+﻿using Ninject;
+using SCM_System.DAL;
+using SCM_System.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace SCM_System.API.Controllers
 {
+    [RoutePrefix("api/system")]
     public class SystemController : ApiController
     {
+        [Inject]
+        //通用模块
+        public DAL_SystemModuel<BaseModel> DAL_SystemModuel { get; set; }
+
+        [HttpPost]
+        [Route("PostLogin")]
+        public dynamic PostLogin(dynamic users)
+        {
+            if (users == null)
+            {
+                return null;
+            }
+            try
+            {
+                if (DAL_SystemModuel.Login(new Users() { UserLoginName = users.UserLoginName.Value, UserLoginPwd = users.UserLoginPwd.Value }))
+                {
+
+                    if (users.UserLoginState.Value)
+                    {
+                        return new Users() { UserLoginName = users.UserLoginName.Value, UserLoginPwd = users.UserLoginPwd.Value };
+                    }
+                    return new Users() { UserLoginName = users.UserLoginName.Value };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
         public void Options() { }  //这是预请求
     }
 }
--- SCM_System.DAL/DAL_SystemModuel.cs
+++ SCM_System.DAL/DAL_SystemModuel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 using SCM_System.Model;
 namespace SCM_System.DAL
 {
-    class DAL_SystemModuel<T> : DAL_UniversalModuel<T>, IDAL.IDAL_UniversalModuel<T> where T : BaseModel
+    public class DAL_SystemModuel<T> : DAL_UniversalModuel<T>, IDAL.IDAL_UniversalModuel<T> where T : class
     {
+        public bool Login(Users user)
+        {
+            if (entities.Users.Where(a => a.UserLoginName == user.UserLoginName && a.UserLoginPwd == user.UserLoginPwd).ToList().Count()>0)
+                return true;
+            return false;
+        }
     }
 }
